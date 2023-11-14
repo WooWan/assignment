688e4b84a654c1704eece7051f89fdc6d571a6ee
WooWan <woohobi@gmail.com>
2023-11-14 19:24:29 +0900

    chore: typescript로 타겟 언어 변경

--- relay.config.js
+++ relay.config.js
@@ -3,7 +3,7 @@ module.exports = {
   // ...
   // Configuration options accepted by the `relay-compiler` command-line tool and `babel-plugin-relay`.
   src: "./src",
-  language: "javascript", // "javascript" | "typescript" | "flow"
+  language: "typescript", // "javascript" | "typescript" | "flow"
   schema: "./data/schema.graphql",
   excludes: ["**/node_modules/**", "**/__mocks__/**", "**/__generated__/**"],
 };
