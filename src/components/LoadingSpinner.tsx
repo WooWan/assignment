b5207fbb08ea72796db176e8fb2a0a8e4d0abbf5
WooWan <woohobi@gmail.com>
2023-11-16 19:21:06 +0900

    design: 로딩 스피너 작게 변경

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -5,7 +5,7 @@ import { cn } from '../lib/utils';
 type Props = {} & React.HTMLAttributes<HTMLDivElement>;
 
 function LoadingSpinner({ className }: Props) {
-  return <Loader2Icon className={cn('h-4 w-4 animate-spin', className)} />;
+  return <Loader2Icon className={cn('h-3 w-3 animate-spin', className)} />;
 }
 
 export default LoadingSpinner;
