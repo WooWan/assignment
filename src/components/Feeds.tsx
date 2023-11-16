d1cfed8baab523f869c49d45012fb229de330bfc
WooWan <woohobi@gmail.com>
2023-11-16 19:25:52 +0900

    refactor: 데이터 refetch를 통해 페칭

--- src/components/Feeds.tsx
+++ src/components/Feeds.tsx
@@ -2,6 +2,8 @@ import { graphql } from 'babel-plugin-relay/macro';
 import { useLazyLoadQuery } from 'react-relay';
 import { FeedsQuery as FeedsQueryType } from './__generated__/FeedsQuery.graphql';
 import SearchResults from './SearchResults';
+import FeedsHeader from './FeedsHeader';
+import { useState } from 'react';
 
 const FeedsQuery = graphql`
   query FeedsQuery($cursor: String) {
@@ -11,8 +13,18 @@ const FeedsQuery = graphql`
 
 function Feeds() {
   const data = useLazyLoadQuery<FeedsQueryType>(FeedsQuery, {});
+  const [searchText, setSearchText] = useState('');
 
-  return <SearchResults search={data} />;
+  const onSearchInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchText(event.target.value);
+  };
+
+  return (
+    <div className="mx-auto flex max-w-md flex-col items-start">
+      <FeedsHeader searchText={searchText} handleTextChange={onSearchInputChange} />
+      <SearchResults search={data} query={searchText} />
+    </div>
+  );
 }
 
 export default Feeds;
--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -2,12 +2,13 @@ import { graphql } from 'babel-plugin-relay/macro';
 import { SearchResultsFragment$key } from './__generated__/SearchResultsFragment.graphql';
 import { usePaginationFragment } from 'react-relay';
 import Repository from './Repository';
-import { useState, useTransition } from 'react';
+import { useEffect, useTransition } from 'react';
 import { cn } from '../lib/utils';
-import FeedsHeader from './FeedsHeader';
+import LoadingSpinner from './LoadingSpinner';
 
 type Props = {
   search: SearchResultsFragment$key;
+  query: string;
 };
 
 const SearchResultsFragment = graphql`
@@ -28,38 +29,40 @@ const SearchResultsFragment = graphql`
   }
 `;
 
-function SearchResults({ search }: Props) {
-  const { data, loadNext, hasNext, refetch } = usePaginationFragment(SearchResultsFragment, search);
+function SearchResults({ search, query }: Props) {
+  const { data, loadNext, hasNext, refetch, isLoadingNext } = usePaginationFragment(SearchResultsFragment, search);
   const [isPending, startTransition] = useTransition();
-  const [searchText, setSearchText] = useState('');
+
+  useEffect(() => {
+    startTransition(() => {
+      refetch({
+        query: query
+      });
+    });
+  }, [query, refetch]);
 
   const onLoadMore = () => {
     loadNext(3);
   };
 
-  const onSearchInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchText(event.target.value);
-    startTransition(() => {
-      refetch({
-        query: event.target.value
-      });
-    });
-  };
-
   return (
-    <div className="mx-auto flex max-w-md flex-col items-start">
-      <FeedsHeader searchText={searchText} handleTextChange={onSearchInputChange} isPending={isPending} />
-      <main className="flex flex-col">
-        <ul className={cn('mb-3 flex flex-col gap-y-6')}>
-          {data.search.edges?.map((edge, index) => <Repository key={index} repository={edge?.node!} />)}
-        </ul>
-        {hasNext && (
-          <button className="mr-2 self-end rounded-md border-4 border-orange-100 px-2 py-1" onClick={onLoadMore}>
-            더 보기
-          </button>
-        )}
-      </main>
-    </div>
+    <main className="flex flex-col">
+      <ul className={cn('mb-3 flex flex-col gap-y-6')}>
+        {data.search.edges?.map((edge, index) => (
+          <Repository key={index} repository={edge?.node!} isPending={isPending} />
+        ))}
+      </ul>
+      {hasNext && (
+        <button
+          disabled={isLoadingNext}
+          className="mr-2 inline-flex items-center gap-x-1 self-end rounded-md border-4 border-orange-100 px-4 py-1"
+          onClick={onLoadMore}
+        >
+          더 보기
+          {isLoadingNext && <LoadingSpinner />}
+        </button>
+      )}
+    </main>
   );
 }
 
