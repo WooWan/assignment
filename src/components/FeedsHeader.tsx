ccda1122d8d6f5c2f9cf61e6c6383de2f97d7d78
WooWan <woohobi@gmail.com>
2023-11-16 19:21:54 +0900

    add: 검색 아이콘 추가

--- src/components/FeedsHeader.tsx
+++ src/components/FeedsHeader.tsx
@@ -1,23 +1,21 @@
-import React from 'react';
-import LoadingSpinner from './LoadingSpinner';
+import { SearchIcon } from 'lucide-react';
 
 type Props = {
   searchText: string;
   handleTextChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
-  isPending: boolean;
 };
 
-function FeedsHeader({ searchText, handleTextChange, isPending }: Props) {
+function FeedsHeader({ searchText, handleTextChange }: Props) {
   return (
-    <header className="flex gap-x-2 py-5">
-      <div className="relative flex items-center justify-between rounded-sm border-[1px] border-black">
+    <header className="flex py-5">
+      <div className="relative flex h-8 items-center justify-between rounded-sm border-[1px] border-black text-sm">
+        <SearchIcon className="absolute left-2 h-4 w-4" />
         <input
           placeholder="저장소를 검색해보세요"
-          className="border-transparent px-2 py-1"
+          className="border-transparent px-8 py-1"
           onChange={handleTextChange}
           value={searchText}
         />
-        {isPending && <LoadingSpinner className="absolute right-2" />}
       </div>
     </header>
   );
